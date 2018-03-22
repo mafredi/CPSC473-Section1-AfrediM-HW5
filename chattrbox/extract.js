c5e96de0a077fc99535e62f76720f18436ebe60d
mafredi <mafredi1@csu.fullerton.edu>
2018-03-21 17:13:21 -0700

    Add files via upload

--- chattrbox/extract.js
+++ chattrbox/extract.js
@@ -1,10 +1,10 @@
 var path = require("path");
 
-var extractFilePath = function (url) {
+var extractFilePath = function(url) {
   var filePath;
   var fileName = "index.html";
 
-  if(url.length > 1){
+  if (url.length > 1) {
     fileName = url.substring(1);
   }
   console.log("The fileName is: " + fileName);
